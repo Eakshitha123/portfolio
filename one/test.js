50503e5096d73181086fca6767f488b0ae289cd3
Eakshitha <eakshitha2005@gmail.com>
2025-06-23 02:39:51 +0000

    m

---
+++ one/test.js
@@ -0,0 +1,21 @@
+// let const var
+// var can be redeclared and updated a global scope variable
+//let variable cannot be redeclared but can be updated , a block scopr variable
+//const variable cannot be redeclared or updated , a block scope variable 
+
+// 2015 before var was used then later es6 was introduced which was const,
+// var num = 123
+
+// {
+//     var num = 345
+//     console.log(num)
+// }
+
+// console.log(num)
+
+let score = "33"
+console.log(typeof(score))
+
+let valueInNumber = Number(23)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
