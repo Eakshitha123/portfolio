157bf5368625215bff36ae548cebecb566815bf1
Eakshitha <eakshitha2005@gmail.com>
2025-06-23 04:04:52 +0000

    m

--- one/test.js
+++ one/test.js
@@ -20,11 +20,71 @@
 // console.log(valueInNumber)
 // console.log(typeof(valueInNumber))
 
-let num = 678
-console.log(typeof num)
+// let num = 678
+// console.log(typeof num)
 
-newnum = Boolean(num)
-console.log(typeof newnum)
+// newnum = Boolean(num)
+// console.log(typeof newnum)
 
-console.log(newnum)
-console.log(typeof NaN)
+// console.log(newnum)
+// console.log(typeof NaN)
+
+
+////////// operations
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue) //-3
+
+// let str1 = "eakshitha"
+// let str2 = " cheppali"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(typeof 1+2/0)
+// console.log("1"+2)
+// console.log(1+9+"9") ==>109
+
+// console.log(true+true)
+// console.log(+"")
+
+// let num1,num2,num3
+// num1 = num2 = num3 = "2"+2
+// console.log(num2)
+
+
+//i = 12;
+// i++
+// console.log(i) //13
+
+//i=12
+//console.log(i++) //12
+
+// console.log(2>1)
+// console.log(true===true)
+
+// console.log("2">1)
+// console.log("02">1)
+
+// num = null
+// newnum = Number(num)
+
+// console.log(typeof newnum)
+
+
+// console.log(null >0 )
+// console.log(null>=0)
+
+// console.log(12==="12")
+
+// console.log(undefined === null) //  true
+
+
+// console.log(undefined == 0)
+// console.log(undefined >0)
+// console.log(undefined<0)
+
+
+let a = 9
+console.log(typeof a)
